cfa32d63302f54659d24ec713424a7eb76df2bde
lee223 <alsilva33330@gmail.com>
2025-01-25 17:46:47 +0000

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,26 @@
-console.log("week 1 - Hello World");
+console.log("hello from insde the main.js file");
+
+/* let myVar; */
+
+const myVar = 42;
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+if(myVarType === "number") {
+    console.log(`will this line run? `)
+} else {
+    console.log(`Or this line?`)
+}
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will this line run? `)
+    } else {
+        console.log(`Or this line?`)
+    }
+}
+
+runNow()
+runNow();
