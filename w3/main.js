9f032d4790d467399c679fc2d933e0b8b6f2d9c6
lee223 <alsilva33330@gmail.com>
2025-01-28 06:52:02 +0000

    commiting because devtools is messing up, using another browser.

--- w3/main.js
+++ w3/main.js
@@ -1,4 +1,4 @@
-function determineHouseHoldpts(numberInHousehold) {
+/* function determineHouseHoldpts(numberInHousehold) {
     console.log("inside the function");
     if (numberInHousehold === 1) {
         carbonFootprintPoints = carbonFootprintPoints + 14;
@@ -19,7 +19,23 @@ function determineHouseHoldpts(numberInHousehold) {
 }
 
 let carbonFootprintPoints = 0;
-const numberInHousehold = 3;
+const numberInHouseHold = 3;
 
 determineHouseHoldpts(3)
-determineHouseHoldpts(4)
+determineHouseHoldpts(4) */
+
+/* my code */
+
+const homeSize = ""
+let homeSizePts = 0
+
+if (homeSize === "small") {
+    homeSizePts === homeSizePts + 10
+} else if(homeSize === "medium") {
+    homeSizePts === homeSizePts + 7
+} else if(homeSize === "large") {
+    homeSizePts === homeSizePts + 4
+} else if(homeSize === "apartment") {
+    homeSizePts === homeSizePts + 2
+}
+console.log(`If your house is ${homeSize} then your carbon footprint is: ${homeSizePts}`)
