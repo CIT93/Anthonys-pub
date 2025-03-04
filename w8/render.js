e1b4f30406e6fa0ef4c0afc38d1eed783104891d
lee223 <alsilva33330@gmail.com>
2025-03-04 23:53:45 +0000

    attempt at coding challenge

--- w8/render.js
+++ w8/render.js
@@ -7,14 +7,16 @@ function renderTBLButtons(index, data){
   btnEdit.textContent = "Edit"
   btnDel.textContent = "Del"
   td.appendChild(btnEdit)
+  
   td.appendChild(btnDel) 
-  btnDel.addEventListener('click', function(e) {
+    btnDel.addEventListener('click', function(e) {
     console.log("hello from inside the delete button")
     console.log(e)
     data.splice(index, 1)
     renderTBL(data)
-  })
-  btnEdit.addEventListener('click', function(e){
+}) 
+
+btnEdit.addEventListener('click', function(e){
 
   })
   return td;
@@ -41,29 +43,24 @@ function renderRow(data) {
 return tbody;
 }
 
-
-// forgot to leave a note on my previous commit, butfor attempting the first code challenge I made the new function, 
-// and cut most of the code that was in TBL into the function, I then created renderRow threw my tbody 
-// variable. I ended recieving a undefined errors. Just Now after looking back, I forgot to create my tbody element in my
-// renderRow(data) function.
 function renderTBL(data) {
+  TBL.innerHTML = "";
+  if (data.length === 0) {
+  return; 
+}
   const table = renderTBlHeading()
   const tbody = renderRow(data)
   table.appendChild(tbody);
   TBL.appendChild(table);
 } 
   
-  // I thought of creating another function and storing all the within
-  // the new function, but after that I was a bit lost on how to proceed.
-  // I kept the orginal code withing the renderTBl function, as I really
-  // as I worried something may break.
   function renderTBlHeading() {
     TBL.innerHTML = "";
     const table = document.createElement("table");
     const thead = document.createElement("thead");
     const tr = document.createElement("tr");
     const headingText = ["Name", "household", "housesize", "total", "actions"]
-    headingText.forEach(function(text) {
+    headingText.forEach(function(text,) {
       const th = document.createElement("th");
       th.textContent = text;
       tr.appendChild(th);
