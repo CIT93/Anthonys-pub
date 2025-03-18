f074da34c799bbfa68534ecd263469b710c66271
lee223 <alsilva33330@gmail.com>
2025-03-18 06:36:18 +0000

    Refactor all functions to be arrow functions

---
+++ w10/global.js
@@ -0,0 +1,9 @@
+const FORM = document.getElementById("form");
+const TBL = document.getElementById("tab-data")
+const FNAME = document.getElementById('firstName');
+const LNAME = document.getElementById('lastName');
+const SUBMIT = document.getElementById('submitError');
+
+
+
+export {FORM, TBL, FNAME, LNAME, SUBMIT};
---
+++ w10/main.js
@@ -0,0 +1,55 @@
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
+import { renderTBL } from "./render.js";
+import { FORM, FNAME, LNAME, SUBMIT } from "./global.js";
+import { saveLS, cfpData } from "./storage.js";
+
+
+
+const start = (first, last, houseHoldMembers, houseSize) => {
+  const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
+  const houseSizePts = determineHouseSizePts(houseSize);
+  const total = houseHoldPTS + houseSizePts;
+  cfpData.push({
+    firstname: first,
+    lastname: last,
+    houseHm: houseHoldMembers,
+    houseS: houseSize,
+    houseHpts: houseHoldPTS,
+    houseSpts: houseSizePts,
+    tot: total,
+  });
+}
+
+const validateField = (event) => {
+  const field = event.target.value;
+  const fieldId = event.target.id;
+  const fieldError = document.getElementById(`${fieldId}Error`);
+
+  if (field === '') {
+    fieldError.textContent = `${fieldId} is required`;
+    event.target.classList.add('invalid');
+  } else {
+    fieldError.textContent = '';
+    event.target.classList.remove('invalid');
+  }
+};
+
+FNAME.addEventListener('blur', validateField);
+LNAME.addEventListener('blur', validateField);
+
+
+renderTBL(cfpData)
+FORM.addEventListener('submit',  (e) => {
+  e.preventDefault();
+
+  if (FNAME.value !== '' && LNAME.value !== '') {
+    SUBMIT.textContent = ''
+    start(FNAME.value, LNAME.value, parseInt(FORM.hhmembers.value), FORM.houses.value)
+    saveLS(cfpData)
+    renderTBL(cfpData)
+    FORM.reset();
+  } else {
+    SUBMIT.textContent = "Form requires first and last name";
+  }
+})
+
