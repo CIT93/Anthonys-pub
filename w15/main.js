44f26ef14b601f5ba73d1a733fe91f7999078f4a
lee223 <alsilva33330@gmail.com>
2025-04-28 06:38:45 +0000

    w15 files ready

--- w15/main.js
+++ w15/main.js
@@ -3,14 +3,14 @@
 function renderPhotos(photos) {
     photos.forEach((img) => { 
         const imgEl = document.createElement("img")
-        imgEl.setAttribute("src", `img.thumbnailURL`)
+        imgEl.setAttribute("src", `https://picsum.photos/seed/${img.id}/150`)
         document.getElementById("Output").appendChild(imgEl);
    })
 }   
 
 async function getPhotos() {
     try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=10`)
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
         const photos = await response.json()
        if (!photos.length) {
         console.log('error on return value')
