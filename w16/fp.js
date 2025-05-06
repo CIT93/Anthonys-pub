30615cab4a608cb7aabd40741f565110e3d7fd43
lee223 <alsilva33330@gmail.com>
2025-05-06 05:42:04 +0000

    w16 setup done

---
+++ w16/fp.js
@@ -0,0 +1,100 @@
+
+
+
+
+
+class FP {
+    constructor(first, last, houseMembers, houseSize, foodChoice, sources, single, double
+    ) {
+        this.first = first
+        this.last = last
+        this.houseMembers = houseMembers
+        this.houseSize = houseSize
+        this.foodChoice = foodChoice
+        this.sources = sources
+        this.single = single
+        this.double = double
+        this.calhouseHoldPoints();
+        this.calhouseSizePoints();
+        this.calfoodChoicePoints();
+        this.calfoodSourcesPoints()
+        this.waterConsuPoints()
+        this.caltotal()
+    }
+    calhouseSizePoints() {
+        if (this.houseSize === "large") {
+            this.houseSizePoints = 10;
+        } else if (this.houseSize === "medium") {
+            this.houseSizePoints = 7;
+        } else if (this.houseSize === "small") {
+            this.houseSizePoints = 4;
+        } else if (this.houseSize === "apt") {
+            this.houseSizePoints = 2;
+        }
+    }
+    calhouseHoldPoints() {
+        if (this.houseMembers === 1) {
+            this.houseHoldPoints = 14;
+        } else if (this.houseMembers === 2) {
+            this.houseHoldPoints = 12;
+        } else if (this.houseMembers === 3) {
+            this.houseHoldPoints = 10;
+        } else if (this.houseMembers === 4) {
+            this.houseHoldPoints = 8;
+        } else if (this.houseMembers === 5) {
+            this.houseHoldPoints = 6;
+        } else if (this.houseMembers === 6) {
+            this.houseHoldPoints = 4;
+        } else if (this.houseMembers > 6) {
+            this.houseHoldPoints = 2;
+        }
+    }
+
+    calfoodChoicePoints() {
+        if (this.foodChoice === "domesticDaily") {
+            this.foodChoicePoints = 10;
+        } else if (this.foodChoice === "domesticOccasional") {
+            this.foodChoicePoints = 6;
+        } else if (this.foodChoice === "vegeterian") {
+            this.foodChoicePoints = 4;
+        } else if (this.foodChoice === "Vegan") {
+            this.foodChoicePoints = 2;
+        }
+    }
+
+    calfoodSourcesPoints() {
+        if (this.sources === 'packed') {
+            this.foodSourcesPoints = 12
+        } else if (this.sources === 'balanced') {
+            this.foodSourcesPoints = 6
+        } else if (this.sources === 'local') {
+            this.foodSourcesPoints = 2;
+        }
+    }
+        
+    waterConsuPoints() {
+            const calculatePoints = (val) => {
+                if (val === 9) return 3;
+                if (val > 4 && val < 9) return 2;
+                if (val > 1 && val < 3) return 1;
+                if (val === 0 || val === null || val === undefined) return 0;
+                return 0;
+            };
+        
+            if (this.double !== null && this.double !== undefined) {
+                this.waterConsuPoints = calculatePoints(this.double) * 2;
+            } else {
+                this.waterConsuPoints = calculatePoints(this.single);
+            }
+        }
+        
+    
+    caltotal() {
+        this.total = this.houseHoldPoints + this.houseSizePoints + this.foodChoicePoints + this.foodSourcesPoints + this.waterConsuPoints
+         
+    }
+}
+
+
+
+export { FP }
---
+++ w16/main.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+import { determineHouseHoldPts, determineHouseSizePts } from "./cfp.js";
+import { renderTBL } from "./render.js";
+import { FORM, FNAME, LNAME, SUBMIT } from "./global.js";
+import { saveLS, cfpData } from "./storage.js";
+import { FP } from "./fp.js";
+
+
+
+
+const validateField = function(event) {
+  const field = event.target.value;
+  const fieldId = event.target.id;
+  const fieldError = document.getElementById(`${fieldId}Error`);
+
+  if (field === '') {
+    fieldError.textContent = `${fieldId} is required`;
+    event.target.classList.add('invalid');
+  } else {
+    fieldError.textContent = '';
+    event.target.classList.remove('invalid');
+  }
+};
+
+FNAME.addEventListener('blur', validateField);
+LNAME.addEventListener('blur', validateField);
+
+
+renderTBL(cfpData)
+FORM.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (FNAME.value !== '' && LNAME.value !== '') {
+    SUBMIT.textContent = ''
+    const fpObj = new FP(
+        FNAME.value, 
+        LNAME.value, 
+        parseInt(e.target.hhmembers.value), 
+        e.target.houses.value, 
+        e.target.Food.value,
+        e.target.sources.value,
+        parseInt(e.target.single.value),
+        parseInt(e.target.double.value))
+        cfpData.push(fpObj)
+        saveLS(cfpData)
+    renderTBL(cfpData)
+    FORM.reset();
+    } else {
+    SUBMIT.textContent = "Form requires first and last name";
+  }
+})
+
+ 
